5f7aa39c7385bf2bdbdaae8874a49b7b79f7ac5e
nospoonvue <nospoonvue@nospoon.nl>
2020-04-29 01:23:51 +0200

    new

---
+++ src/views/Course.vue
@@ -0,0 +1,148 @@
+<template>
+    <div id="course">
+        <Status class="StatusShow" v-bind:img="status" v-show="loading" />
+        <section>
+
+        <span class="image main">
+            <div class="" v-for="item in filterImages" :key="item.Title" >
+                <img v-bind:src="item.url" v-bind:alt="item.Title" />
+            </div>
+            <h1>  {{course.Title}}</h1>
+        </span>
+
+        <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+        <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+
+        <hr class="major" />
+
+        <h2>Interdum sed dapibus</h2>
+        <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+
+        <hr class="major" />
+
+        <h2>Magna etiam veroeros</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+        <p>Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit tristique lorem ipsum dolor.</p>
+
+        <hr class="major" />
+
+        <h2>Lorem aliquam bibendum</h2>
+        <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Curabitur sapien risus, commodo eget turpis at, elementum convallis elit. Pellentesque enim turpis, hendrerit.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
+
+         </section>
+ 
+      
+     
+
+
+    </div>
+</template>
+
+<style >
+
+</style>
+
+<script>
+import axios from 'axios';
+import Status from '@/components/Status.vue'
+import VueMarkdown from 'vue-markdown'
+import { VueAgile } from 'vue-agile'
+
+async function getData(viewStatus)
+{
+        const config = 
+    {
+        headers: 
+        {
+        
+        
+        }
+    }
+
+var settings = {
+    'table': viewStatus.table,
+    'pageSize': viewStatus.pageSize,
+    'offset': viewStatus.offset,
+    'fields': ['Title','ShortDescription', 'Visuals','ECTS'],
+    'filter': '{LinkName}="'+ viewStatus.name + '"'
+}
+//alert(JSON.stringify(settings));
+const formData = new FormData();
+formData.append('data', JSON.stringify(settings));
+formData.append('action', 'view');
+
+viewStatus.loading = true;
+await axios.post (viewStatus.$baseUrl, formData, config)
+        .then(response => 
+        {
+            //viewStatus.data = response.data.records;
+            alert(JSON.stringify(response.data.records[0].fields));
+
+            viewStatus.course = response.data.records[0].fields;
+            
+            viewStatus.loading = false;
+
+        }).catch
+        (
+            function (error) 
+            {
+                console.log("fooout:"+error)
+                //viewStatus.status = error;
+                viewStatus.ready = false;
+                viewStatus.loading = false;
+            }
+        )
+}
+
+export default 
+{
+    name: "course",
+    components: {Status, VueMarkdown,Agile: VueAgile },
+
+    data() 
+    {
+    return {
+        data: null,
+        ready: false,
+        loading: false,
+        status: "",
+        table: "Course",
+        course: {}
+        }
+    },
+    props: 
+    {
+        name: 
+        {
+        type: String,
+        default: 'Prachtig'
+        }
+    },
+    methods:
+    {
+
+    },
+    computed: 
+    {
+        filterImages: function () {
+            return this.course.Visuals.filter(function (image) {
+            return  image.url.includes("1280x416");  
+            })
+        }
+    },
+    created() 
+    {
+     getData(this);
+ 
+    },
+    mounted()
+    {
+       // alert(this.name);
+    }
+}
+
+
+</script>
